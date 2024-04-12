0db6034ab23fb4dafe6cfacda67a08b6dc2a6642
Aimisnotavailable <aim.990111@gmail.com>
2024-04-12 20:02:44 +0800

    Daily Leetcode Questions 12-04-24

--- Hard/42_Trapping_Rain_Water.cpp
+++ Hard/42_Trapping_Rain_Water.cpp
@@ -1,64 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-
+#include<vector>
+#include<climits>
 class Solution {
 public:
     int trap(std::vector<int>& height) {
-        int n = height.size();
-        int trapped = 0;
-        if(n >= 1 && n <= 2 * pow(10,4)){
-            int start = 0;
-            while(true && start < n){
-                if(height[start] == 0){
-                    start++;
-                    continue;
-                }
-                break;  
-            }
-            while(start < n){
-                int r_dim;
-                int l_dim = height[start];
-                int s_apex = -1;
-                int s_apex_ind = -1;
-                std::cout << "ITERATION : ";
-                for(int i = start + 1; i < n; i++){
-                    if(height[i] < 0)
-                        return -1;
-                    if(height[i] >= l_dim){
-                        r_dim = height[i];
-                        std::cout << "start: " << start << " left_dim: " << l_dim << " right_dim: " << r_dim << std::endl;
-                        int v_bound = (l_dim < r_dim) ? l_dim : r_dim;
-                        for(int j = start; j < i; j++){
-                            trapped += (v_bound - height[j]);
-                        }
-                        start = i;
-                        l_dim = r_dim;
-                        s_apex = -1;
-                        s_apex_ind = -1;
-                        continue;
-                    }else{
-                        if(height[i] >= s_apex || s_apex == -1){   
-                            s_apex = height[i];
-                            s_apex_ind = i;
-                        }
-                        if((i + 1) == n && (s_apex_ind - start) > 1){
-                            r_dim = s_apex;
-                            int v_bound = (l_dim < r_dim) ? l_dim : r_dim;
-                            
-                            for(int j = start + 1; j < s_apex_ind; j++){
-                                trapped += (v_bound - height[j]);
-                            }
-                            start = s_apex_ind - 1;
-                            l_dim = r_dim;
-                        }
-                    }
-                }
-                start++;
-            } 
-            
+        int lmax = INT_MIN;
+        int rmax = INT_MIN;
+        int l = 0;
+        int r = height.size() - 1;
+        int res = 0;
+        while(l < r){
+            lmax = std::max(lmax, height[l]);
+            rmax = std::max(rmax, height[r]);
+            res+=(lmax < rmax) ? lmax - height[l++] : rmax - height[r--];
         }
-        height.clear();
-        return trapped;
+
+        return res;
     }
 };
