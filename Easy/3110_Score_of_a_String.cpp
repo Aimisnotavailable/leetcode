315588d52d33161351a80ed90f5bb6915160b4dc
Aimisnotavailable <aim.990111@gmail.com>
2024-06-01 22:25:40 +0800

    Daily Leetcode Questions 01-06-24

---
+++ Easy/3110_Score_of_a_String.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        
+        int score = 0;
+        
+        for(int i = 0; i < s.size() - 1; i++){
+            score += std::abs(s[i] - s[i + 1]);    
+        }
+        
+        return score;
+    }
+};
